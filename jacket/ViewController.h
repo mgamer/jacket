15f1338f9435e429f9d21e3ba3c174a65cd1e9ff
mgamer <michal.lukasiewicz@gmail.com>
2013-01-02 15:23:16 +0100

    Initial Commit

---
+++ jacket/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  jacket
+//
+//  Created by michal lukasiewicz on 1/2/13.
+//  Copyright (c) 2013 jackets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ jacket/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  jacket
+//
+//  Created by michal lukasiewicz on 1/2/13.
+//  Copyright (c) 2013 jackets. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
