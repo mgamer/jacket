c8f6ff1f7f48efdd0e493a6771365d0db5004940
mgamer <michal.lukasiewicz@gmail.com>
2013-01-03 11:08:40 +0100

    hello world

--- jacket/ViewController.h
+++ jacket/ViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Network.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <RKRequestDelegate>
 
 @end
--- jacket/ViewController.m
+++ jacket/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 #import "BlocksKit.h"
+#import "Network.h"
+#import "RKLog.h"
+#import "HLDeferred.h"
 
 @interface ViewController ()
 
@@ -21,18 +24,52 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 
-    alert = [[UIAlertView alloc] initWithTitle:@"T" message:@"ASd"];
-    [alert addButtonWithTitle:@"hello" handler:^{
-        NSLog(@"niezle");
+
+
+//    RKLogConfigureByName("RestKit", RKLogLevelTrace);
+//    RKLogConfigureByName("RestKit/ObjectMapping", RKLogLevelTrace);
+//    RKLogConfigureByName("RestKit/Network", RKLogLevelTrace);
+
+    RKClient *client = [RKClient clientWithBaseURLString:@"http://www.wp.pl"];
+
+    HLDeferred *deferred = [HLDeferred new];
+
+    [deferred then:(id) ^(id response) {
+        alert = [[UIAlertView alloc] initWithTitle:@"Data from server" message:[response bodyAsString]];
+        [alert addButtonWithTitle:@"ok" handler:^{
+            NSLog(@"niezle");
+        }];
+
+        [alert show];
     }];
 
-    [alert show];
+    [client get:@"https://gist.github.com/raw/4442387/3f131c40c6ea5b2ffbd78670ccbbfddd3f8e7f30/gistfile1.txt" usingBlock:^(RKRequest *request) {
+        request.delegate = self;
+
+        request.onDidLoadResponse = ^(RKResponse *response) {
+            NSLog(@"response:%@", response);
+            [deferred takeResult:response];
+        };
+
+        request.onDidFailLoadWithError = ^(NSError *error) {
+            NSLog(@"error2:%@", error);
+        };
+    }];
 
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void)request:(RKRequest *)request didReceiveResponse:(RKResponse *)response {
+    NSLog(@"ok");
+}
+
+- (void)request:(RKRequest *)request didFailLoadWithError:(NSError *)error {
+    NSLog(@"fail");
 }
 
 @end
